4305aca6bf87c36877985cad930a8743744d67a1
omega050 <mauro-jr50@hotmail.com>
2024-08-07 16:23:42 -0300

    Starting Admin Branch

--- gastrobar/admin.py
+++ gastrobar/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from gastrobar.models import Pedido, ItemPedido, Prato
 
-# Register your models here.
+class ItensPedido(admin.ModelAdmin):
+    list_display = ('numero_pedido', 'item', 'quantidade')
+admin.site.register(ItemPedido, ItensPedido)
+
+class Pratos(admin.ModelAdmin):
+    list_display = ('nome', 'preco', 'descricao', 'disponivel')
+admin.site.register(Prato, Pratos)
+
+class Pedidos(admin.ModelAdmin):
+    list_display = ('data', 'hora', 'preco')
+admin.site.register(Pedido, Pedidos)
